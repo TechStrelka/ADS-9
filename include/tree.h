b98d23a5cec3626c72ea79d8831115063b5a5a8c
Egor Soluynov <167007583+TechStrelka@users.noreply.github.com>
2026-06-05 23:45:17 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,29 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
-#endif  // INCLUDE_TREE_H_
+#include <vector>
+
+struct Node {
+    char value;
+    std::vector<Node*> children;
+    ~Node();
+};
+
+class PMTree {
+private:
+    Node* root;
+    size_t element_count;
+    void buildTree(Node* node, std::vector<char> elements);
+
+public:
+    PMTree(const std::vector<char>& in);
+    ~PMTree();
+    Node* getRoot() const;
+    size_t getElementCount() const;
+};
+
+std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
+std::vector<char> getPerm1(const PMTree& tree, int num);
+std::vector<char> getPerm2(const PMTree& tree, int num);
+
+#endif
